cbe939db51bea3f76ba91a389039b9707818c8cf
Michal Delura <deluramichal@gmail.com>
2020-08-22 17:27:52 +0200

    Save recipe to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,31 @@ import Box from '@material-ui/core/Box';
 import { Recipe } from './models/Recipe';
 import RecipeEditor from './components/RecipeEditor';
 import Brewer from './components/Brewer';
+import { useDebouncedCallback } from 'use-debounce';
 
 const initialCoffeeAmount = 20;
 const initialWaterAmount = 300;
 
+const recipeKey = 'recipe';
+
 const App = () => {
-    const [recipe, setRecipe] = useState(new Recipe(initialCoffeeAmount, initialWaterAmount));
+    const savedRecipe = localStorage.getItem(recipeKey);
+    const initialRecipe = savedRecipe ? (JSON.parse(savedRecipe) as Recipe) : new Recipe(initialCoffeeAmount, initialWaterAmount);
+    const [recipe, setRecipe] = useState(initialRecipe);
+
+    const [saveToLocalStorageDebounced] = useDebouncedCallback((recipe: Recipe) => localStorage.setItem(recipeKey, JSON.stringify(recipe)), 500, {
+        maxWait: 2000,
+    });
+
+    const handleRecipeChanged = (recipe: Recipe) => {
+        setRecipe(recipe);
+        saveToLocalStorageDebounced(recipe);
+    };
 
     return (
         <Container maxWidth="sm">
             <Box>
-                <RecipeEditor recipe={recipe} onRecipeChanged={setRecipe} />
+                <RecipeEditor recipe={recipe} onRecipeChanged={handleRecipeChanged} />
                 <Brewer recipe={recipe} />
             </Box>
         </Container>
